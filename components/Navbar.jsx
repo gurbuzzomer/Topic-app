4c50c88b9229dc185d8178c8518a4ce9441b6dbd
omerfaruk-gurbuz <gurbuzzomer@gmail.com>
2024-06-24 19:22:40 +0300

    @omerfaruk-gurbuz header trying resolve 2 (hatice)

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <nav className="flex justify-between items-center bg-indigo-600 px-8 py-3">
       <Link className="text-white font-bold" href={"/"}>
-      Omer Faruk Gurbuz{"&#39"}s
+      Hatice Gurbuz{"&#39"}s
       </Link>
       <Link href={"/addTopic"}>
       <button className="bg-white text-indigo-600 font-bold py-2 px-4 rounded-md hover:bg-purple-100">
