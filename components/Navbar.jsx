4979d8e94862f7e7b0565b5c5b95b1dfce456fd6
omerfaruk-gurbuz <gurbuzzomer@gmail.com>
2024-06-24 19:28:32 +0300

    navbar changed

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,11 +4,11 @@ export default function Navbar() {
   return (
     <nav className="flex justify-between items-center bg-indigo-600 px-8 py-3">
       <Link className="text-white font-bold" href={"/"}>
-        Hatice Gurbuz{"&#39"}s
+        muhammet Gurbuz{"&#39"}s
       </Link>
       <Link href={"/addTopic"}>
       <button className="bg-white text-indigo-600 font-bold py-2 px-4 rounded-md hover:bg-purple-100">
-          Add Topic
+          Add muhammet
       </button>
       </Link>
     </nav>
