0ffccfe0d34cdac95d07d84327ac173e11c878f7
omerfaruk-gurbuz <gurbuzzomer@gmail.com>
2024-06-24 14:15:03 +0300

    ready for deploy

--- libs/mongodb.js
+++ libs/mongodb.js
@@ -9,7 +9,7 @@ const connectMongoDB = async () => {
 
   try {
     const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
-    const connectionString = `mongodb+srv://integrationninjas:${password}@test.vjuinp4.mongodb.net/?retryWrites=true&w=majority`; // clustore url
+    const connectionString = `mongodb+srv://test-user:${password}@devcluster.xx5ayeu.mongodb.net/?retryWrites=true&w=majority&appName=DevCluster`
     await mongoose.connect(connectionString, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
