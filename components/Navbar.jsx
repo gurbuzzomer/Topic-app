9787e5a02c9e629c0b1e0ba378303c85ab9d3e5c
omerfaruk-gurbuz <gurbuzzomer@gmail.com>
2024-06-24 15:59:30 +0300

    navbar turned green

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,12 +2,12 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="flex justify-between items-center bg-purple-600 px-8 py-3">
+    <nav className="flex justify-between items-center bg-green-600 px-8 py-3">
       <Link className="text-white font-bold" href={"/"}>
         Omer Faruk Gurbuz&#39s
       </Link>
       <Link href={"/addTopic"}>
-      <button className="bg-white text-purple-600 font-bold py-2 px-4 rounded-md hover:bg-purple-100">
+      <button className="bg-white text-green-600 font-bold py-2 px-4 rounded-md hover:bg-purple-100">
           Add Topic
       </button>
       </Link>
